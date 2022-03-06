6d0b3e6d706d8bdbc411d0d05291ae12b04f59ad
sahinboga <bogasahin967@gmail.com>
2022-03-06 11:45:29 +0300

    Added Entityframework

---
+++ ElectricityMeter/DataAccess/Concrete/ElectricityMeterContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+	public class ElectricityMeterContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Electricity_Meter;Trusted_Connection=true");
+		}
+
+		
+	}
+}
