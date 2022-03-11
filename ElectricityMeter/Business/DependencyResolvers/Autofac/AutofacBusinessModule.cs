3fa103ff57ebc07e9347dc1b0e8e85504090e044
sahinboga <bogasahin967@gmail.com>
2022-03-11 11:11:11 +0300

    Added autofac and dependency ninject

---
+++ ElectricityMeter/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+	public class AutofacBusinessModule:Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+
+			builder.RegisterType<CompanyManager>().As<ICompanyService>().SingleInstance();
+			builder.RegisterType<EfCompanyDal>().As<ICompanyDal>().SingleInstance();
+
+			builder.RegisterType<MeterManager>().As<IMeterService>().SingleInstance();
+			builder.RegisterType<EfMeterDal>().As<IMeterDal>().SingleInstance();
+
+			builder.RegisterType<CityManager>().As<ICityService>().SingleInstance();
+			builder.RegisterType<EfCityDal>().As<ICityDal>().SingleInstance();
+
+			builder.RegisterType<DistrictManager>().As<IDistrictService>().SingleInstance();
+			builder.RegisterType<EfDistrictDal>().As<IDistrictDal>().SingleInstance();
+
+			/*var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+			builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+				.EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions()
+				{
+					Selector = new AspectInterceptorSelector()
+				}).SingleInstance();*/
+		}
+	}
+}
---
+++ ElectricityMeter/Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Ninject
+{
+	public class BusinessModule:NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<ICompanyService>().To<CompanyManager>().InSingletonScope();
+			Bind<ICompanyDal>().To<EfCompanyDal>().InSingletonScope();
+
+			Bind<IMeterService>().To<MeterManager>().InSingletonScope();
+			Bind<IMeterDal>().To<EfMeterDal>().InSingletonScope();
+
+			Bind<ICityService>().To<CityManager>().InSingletonScope();
+			Bind<ICityDal>().To<EfCityDal>().InSingletonScope();
+
+			Bind<IDistrictService>().To<DistrictManager>().InSingletonScope();
+			Bind<IDistrictDal>().To<EfDistrictDal>().InSingletonScope();
+		}
+	}
+}
